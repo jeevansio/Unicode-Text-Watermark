18686334b63eb00d7565e1f4247f90a04c5503ff
jeevansio <jeevansio55@gmail.com>
2019-12-13 20:11:45 +0800

    Update utm.py
    add extract()

--- utm.py
+++ utm.py
@@ -38,14 +38,14 @@ trans = {   u"\u002d": u"\u2010", u"\u003b": u"\u037e",
             u"\u006a": u"\u0458", u"\u006c": u"\u217c",
             u"\u0076": u"\u2174", u"\u0078": u"\u2179"}
 
-detrans = { u"\u2010": u"\u002d", u"\u037e": u"\u003b",
-            u"\u216d": u"\u0043", u"\u216e": u"\u0044",
-            u"\u212a": u"\u004b", u"\u216c": u"\u004c",
-            u"\u216f": u"\u004d", u"\u2164": u"\u0056",
-            u"\u2169": u"\u0058", u"\u217d": u"\u0063",
-            u"\u217e": u"\u0064", u"\u2170": u"\u0069",
-            u"\u0458": u"\u006a", u"\u217c": u"\u006c",
-            u"\u2174": u"\u0076", u"\u2179": u"\u0078"}
+# detrans = { u"\u2010": u"\u002d", u"\u037e": u"\u003b",
+#             u"\u216d": u"\u0043", u"\u216e": u"\u0044",
+#             u"\u212a": u"\u004b", u"\u216c": u"\u004c",
+#             u"\u216f": u"\u004d", u"\u2164": u"\u0056",
+#             u"\u2169": u"\u0058", u"\u217d": u"\u0063",
+#             u"\u217e": u"\u0064", u"\u2170": u"\u0069",
+#             u"\u0458": u"\u006a", u"\u217c": u"\u006c",
+#             u"\u2174": u"\u0076", u"\u2179": u"\u0078"}
 
 # whitespace symbols: Space, En quad, Three-per-em space,
 #                     Four-per-em space, Punctuation space, Thin space,
@@ -60,44 +60,87 @@ def usage():
     print("Unicode Text Watermark Embedding Tool")
     print("\n"*8)
 
+# get a whitespace index from blank_space
+def getindex(str):
+	for i in range(len(blank_space)):
+		if blank_space[i] == str:
+			return i
+
+# joint strings in list
+def magic(wm):
+	wm = reduce(lambda x, y:x+y , wm) # wm = ['0110']
+	return wm
+
+# decimal to binary
+def getbinstr(num):
+	num = bin(num)  # 0->'0b0', str
+	num = num[2:]   #  ->'0'
+	lenn = len(num)
+	num = '0'*(3-lenn) + num #'000'
+	return num
+
+# watermark embedment
 def embed(text_string, bitstream):
+    chr_addr = 0
     bit_addr = 0
     bit_len = len(bitstream)
+    text_watermarked = text_string
 
     # scan string for confusable character or blank space
-    for character in text_string:
+    while(chr_addr < len(text_watermarked)):
         # find a confusable character, read 1 bit from bitstream
-        if character in original_code:
-            print("confusable char: %s" %(character))
+        if text_watermarked[chr_addr] in original_code:
+            print("confusable char: %s" %(text_watermarked[chr_addr]))
             if(bitstream[bit_addr%bit_len] == "1"):
-                character = trans[character]
-            print(bitstream[bit_addr%bit_len], character)
+                text_watermarked = text_watermarked.replace(text_watermarked[chr_addr], trans[text_watermarked[chr_addr]])
+            print(bitstream[bit_addr%bit_len], text_watermarked[chr_addr])
             bit_addr = bit_addr + 1
         # find a blank space, read 3 bits from bitstream
-        elif character in blank_space:
-            print("blank space: %s" %(character))
+        elif text_watermarked[chr_addr] in blank_space:
+            print("blank space: %s" %(text_watermarked[chr_addr]))
+            # from bits to num
             temp = 4*bitstream[bit_addr%bit_len] + 2*bitstream[(bit_addr+1)%bit_len] + bitstream[(bit_addr+2)%bit_len]
-            character = blank_space[temp]
+            text_watermarked = text_watermarked.replace(text_watermarked[chr_addr], blank_space[temp])
             bit_addr = bit_addr + 3
         else:
             continue
-
+        chr_addr = chr_addr + 1
+    
     print(text_string)
-    return text_string
+    print(text_watermarked)
+    return text_watermarked
 
-def extract():
+# watermark extraction
+def extract(text_watermarked):
+    wm = ['']
+    
+    for ch in text_watermarked:
+        if ch in blank_space:
+			print('find str:%s' %(ch))
+			index = getindex(ch) # int
+			index = getbinstr(index) # str,'000'~'111'
+			wm.append(index)
+        elif ch in original_code:
+            wm.append('0')
+        elif ch in duplicate_code:
+            wm.append('1')
+        else:
+            continue
+
     print("\n")
+    print(wm)
+    return wm
 
 def hash(value, key):
-	hm = hmac.new(key,value)
+	hm = hmac.new(key, value)
 	wm = hm.hexdigest( ) 
-	str=''
+	str = ''
 	for i in range(len(wm)):
-		temp=bin(int((wm[i]),16))
-		temp=temp[2:]
-		lent=len(temp)
-		temp='0'*(4-lent)+temp
-		str=str+temp
+		temp = bin(int((wm[i]), 16))
+		temp = temp[2:]
+		lent = len(temp)
+		temp = '0'*(4-lent) + temp
+		str = str + temp
 	return str
 
 def main():
@@ -116,11 +159,11 @@ def main():
         # usage()
 
     for o,a in opts:
-        if o in ("-h", "--HELP"):
+        if o in ("-h", "--help"):
             usage()
-        elif o in ("-i", "--input"):
+        elif o in ("-e", "--embed"):
             embedment = 1       
-        elif o in ("-i", "--input"):
+        elif o in ("-x", "--extract"):
             extraction = 1
         elif o in ("-i", "--input"):
             file_path = a
@@ -135,19 +178,28 @@ def main():
     # temporary settings for test purpose
     file_path = "original_text.txt"
     output_path = "watermarked_text.txt"
-    text_string = open(file_path)
-    text_string = text_string.read()
     # text_string = text_string.decode('utf-8')
     # text_string = "abcdedsiugxxxeusrigrxsixgjsxjgszznxaxcvrslkxxfodxxlxxi"
-    watermark = list("01")
+    watermark = list("01")    
+    
+    # read from file(read only mode)
+    f = open(file_path, 'rb')
+    text_string = f.read()
+    f.close()
+    text_string = text_string.decode('utf-8')
+
 
     if embedment:
         text_watermarked = embed(text_string, watermark)
-        f2 = open(output_path, 'w+')
+        f2 = open(output_path, 'w+', encoding='utf-8')
         f2.write(text_watermarked)
         f2.close()
     elif extraction:
-        extract()
+        watermark_extracted = extract(text_string)
+        print(watermark_extracted)
+        # bitstram to hash
+        #
+        #
     
 
 main()
