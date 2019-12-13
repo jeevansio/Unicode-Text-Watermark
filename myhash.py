84e58b758fdf28a4472b7eb038893c8119bebdce
Carrieono <35685393+Carrieono@users.noreply.github.com>
2019-12-13 11:28:50 +0800

    fix the problem of losing bits when transforming
    The last didn't  acknowledge that when bin(int(x,16)),it becomes this :
    '5'   -> '0b101'
    '1'  ->  '0b1'
    'a'   ->'0b1010'

--- myhash.py
+++ myhash.py
@@ -10,6 +10,9 @@ text=b'ILOVEU'*100
 
 import hmac
 
+'''
+this is wrong since I didn't realize that the function will lose some bits
+
 hm = hmac.new(passwd,text)
 wm = hm.hexdigest()
 print('len=%d,hexwm=%s'%(len(wm),wm))
@@ -17,11 +20,29 @@ print('len=%d,hexwm=%s'%(len(wm),wm))
 wm2=bin(int(wm, 16))
 #the transformation above leaves "0b" so we have to del the first 2
 wm2=wm2[2:]
-print('len=%d,binwm=%s'%(len(wm2),str(wm2)))
+print('len=%d,binwm=%s'%(len(wm2),str(wm2)))'''
 
 #test shows that output type=str,len=128bit(after del)
 
+'''This is a good one!!'''
+def myhash(value,key):
+	hm = hmac.new(key,value)
+	wm = hm.hexdigest( ) 
+	#len=32,str,hex,eg"5157f56c40e7d4964bf9bca8e4fb9a63"
+	str=''
+	for i in range(len(wm)):
+		temp=bin(int((wm[i]),16))
+		temp=temp[2:]
+		lent=len(temp)
+		temp='0'*(4-lent)+temp
+		str=str+temp
+	print(str)
+	print(len(str))
+	return str
 
+#finally output is str,type=str,len=128
+
+watermark=myhash(text,passwd)
 
 
 
